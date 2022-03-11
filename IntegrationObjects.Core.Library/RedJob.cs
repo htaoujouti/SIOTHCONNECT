7d6e4c7e3726575de3f3c5ba5d7f5fac5b9bd783
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-03-11 16:45:45 +0100

    create a core library to facilitate the debug and use the redjob class for the logic and core of the program,

---
+++ IntegrationObjects.Core.Library/RedJob.cs
@@ -0,0 +1,41 @@
+﻿using IntegrationObjects.Common.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IntegrationObjects.Core.Library
+{
+    public class RedJob
+    {
+        public RedJob(ConfigurationFile configurationFile )
+        {
+            // based con config -->Start threads 
+            Thread thread = new Thread(() => PingMethodOnly());
+            thread.Start();
+            thread.Name = "";
+            thread.IsBackground = true;
+
+        }
+        private void PingMethodOnly()
+        {
+            while (true)
+            {
+                try
+                {
+                    // your code here
+
+                    // log stats
+                }
+                catch (Exception Ex)
+                {
+                    //log
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
