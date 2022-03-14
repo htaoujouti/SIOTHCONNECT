0a9ae0ebbcd8930a1c449dbfe3fdeab5189c9049
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-03-14 17:23:50 +0100

    start to implement ping and implementing conditions

--- IntegrationObjects.Core.Library/RedJob.cs
+++ IntegrationObjects.Core.Library/RedJob.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Ping = IntegrationObjects.Common.Library.Ping;
 
 namespace IntegrationObjects.Core.Library
 {
@@ -12,26 +14,62 @@ namespace IntegrationObjects.Core.Library
     {
         public RedJob(ConfigurationFile configurationFile )
         {
-            // based con config -->Start threads 
-            Thread thread = new Thread(() => PingMethodOnly());
-            thread.Start();
-            thread.Name = "";
-            thread.IsBackground = true;
+
+            if (configurationFile.IsPingEnabled && !configurationFile.IsTCPingEnabled)
+            {
+                // based con config -->Start threads 
+                Thread thread = new Thread(() => PingMethodOnly(configurationFile));
+                thread.Start();
+                thread.Name = "";
+                thread.IsBackground = true;
+            }
+            else if(!configurationFile.IsPingEnabled && configurationFile.IsTCPingEnabled)
+            {
+
+            }
+            else if(configurationFile.IsPingEnabled && configurationFile.IsTCPingEnabled){
+
+            }
+            else
+            {
+
+            }
 
         }
-        private void PingMethodOnly()
+        public void PingMethodOnly(ConfigurationFile config)
         {
             while (true)
             {
                 try
                 {
                     // your code here
+                    Ping pingSender = new Ping();
+                    pingSender = config.ping;
+                    long totalTime = 0;
+                    
+                        for (int i = 0; i < pingSender.retry; i++)
+                        {
+                            PingReply reply = pingSender.Send(pingSender.IPAddress, pingSender.timeOut);
+                            if (reply.Status == IPStatus.Success)
+                            {
+                                totalTime += reply.RoundtripTime;
+
+                                Console.WriteLine(reply.Status);
+                           
+                            }
+                        }
+                        
+                    
+                    
+                        
+                    
 
                     // log stats
                 }
                 catch (Exception Ex)
                 {
                     //log
+                    Console.WriteLine("Invalid Ip Address");
                 }
                 Thread.Sleep(1000);
             }
