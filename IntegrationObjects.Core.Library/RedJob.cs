35ea8341421b99f1cc954ca5350150074c121705
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-03-17 15:41:54 +0100

    my try

--- IntegrationObjects.Core.Library/RedJob.cs
+++ IntegrationObjects.Core.Library/RedJob.cs
@@ -14,27 +14,44 @@ namespace IntegrationObjects.Core.Library
 {
     public class RedJob
     {
-        public RedJob(ConfigurationFile configurationFile )
+        public RedJob(ConfigurationFile configurationFile)
         {
 
             if (configurationFile.IsPingEnabled && !configurationFile.IsTCPingEnabled)
-            {
-                // based con config -->Start threads 
-                Thread thread = new Thread(() => PingMethodOnly(configurationFile));
-                thread.Start();
-                thread.Name = "";
-                thread.IsBackground = true;
+            {   // based con config -->Start threads 
+                while (true)
+                {
+                    Thread thread = new Thread(() => PingMethodOnly(configurationFile));
+                    thread.Start();
+                    thread.Name = "PINGThread";
+                    thread.IsBackground = true;
+                    Thread.Sleep(1000);
+                }
             }
-            else if(!configurationFile.IsPingEnabled && configurationFile.IsTCPingEnabled)
-            {
-                // based con config -->Start threads 
-                Thread thread = new Thread(() => TCPingMethodOnly(configurationFile));
-                thread.Start();
-                thread.Name = "";
-                thread.IsBackground = true;
+            else if (!configurationFile.IsPingEnabled && configurationFile.IsTCPingEnabled)
+            {   // based con config -->Start threads 
+                while (true)
+                {
+                    Thread thread = new Thread(() => TCPingMethodOnly(configurationFile));
+                    thread.Start();
+                    thread.Name = "TCPINGthread";
+                    thread.IsBackground = true;
+                    Thread.Sleep(1000);
+                }
             }
-            else if(configurationFile.IsPingEnabled && configurationFile.IsTCPingEnabled){
-
+            else if (configurationFile.IsPingEnabled && configurationFile.IsTCPingEnabled)
+            {
+                while (true)
+                {
+                    Thread Thread = new Thread(() => { 
+                        PingMethodOnly(configurationFile);
+                        TCPingMethodOnly(configurationFile);
+                    });
+                    Thread.Start();
+                    Thread.Name = "Ping&TCPingThread";
+                    Thread.IsBackground = true;
+                    Thread.Sleep(1000);                                     
+                }
             }
             else
             {
@@ -44,69 +61,55 @@ namespace IntegrationObjects.Core.Library
         }
         public void PingMethodOnly(ConfigurationFile config)
         {
-            while (true)
-            {
-                try
+            try
+            {   // your code here
+                Ping PingSender = new Ping();
+                long TotalTime = 0;
+
+                for (int i = 0; i < config.ping.retry; i++)
                 {
-                    // your code here
-                    Ping PingSender = new Ping();                   
-                    long TotalTime = 0;
-                    int Success = 0;
-                    
-                    for (int i = 0; i < config.ping.retry; i++)
+                    PingReply reply = PingSender.Send(config.ping.IPAddress, config.ping.timeOut);
+                    if (reply.Status == IPStatus.Success)
                     {
-                        PingReply reply = PingSender.Send(config.ping.IPAddress, config.ping.timeOut);
-                        if (reply.Status == IPStatus.Success)
-                        {
-                            TotalTime += reply.RoundtripTime;
-                            Success++;                                                                                       
-                        }
+                        TotalTime += reply.RoundtripTime;
                     }
-                    Success = (Success / config.ping.retry)*100;
-                    TotalTime = (TotalTime / config.ping.retry);
-
-
-
-
-
-                    // log stats
+                    Console.WriteLine(reply.Status.ToString());
                 }
-                catch (Exception Ex)
-                {
-                    //log
-                    Console.WriteLine("Invalid Ip Address");
-                }
-                Thread.Sleep(1000);
+                TotalTime = (TotalTime / config.ping.retry);
+                // log stats
             }
+            catch (Exception Ex)
+            {   //log
+                Console.WriteLine("Invalid Ip Address");
+                
+            }
+            Thread.Sleep(1000);
         }
         public void TCPingMethodOnly(ConfigurationFile config)
         {
-            while (true)
+            try
             {
-                try
-                {
-                    for (int i = 0; i < config.tCPing.retry; i++)
-                    {
-                        var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                        sock.Blocking = true;
+                var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                var stopwatch = new Stopwatch();
+                double t = stopwatch.Elapsed.TotalMilliseconds;
 
-                        var stopwatch = new Stopwatch();
-
-                        // Measure the Connect call only
-                        stopwatch.Start();
-                        sock.Connect(config.tCPing.IPAddress, config.tCPing.port);
-                        stopwatch.Stop();
-                        double t = stopwatch.Elapsed.TotalMilliseconds;
-                        Console.WriteLine("{0:0.00}ms", t);
-                        sock.Close();
-                    }
+                for (int i = 0; i < config.tCPing.retry; i++)
+                {   // Measure the Connect call only
+                    stopwatch.Start();
+                    sock.Connect(config.tCPing.IPAddress, config.tCPing.port);
+                    stopwatch.Stop();
+                    if (sock.Connected) Console.WriteLine("Success");
+                    else Console.WriteLine("failed");
+                    Console.WriteLine("{0:0.00}ms", t);
+                    sock.Close();
                 }
-                catch (Exception Ex)
-                {
-                    Console.WriteLine("Invalid Ip Address or ports");
-                }
-                Thread.Sleep(1000);
             }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("Invalid Ip Address or ports");
+            }
+                            Thread.Sleep(1000);
+
         }
 
     }
